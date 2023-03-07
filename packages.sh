fa4d5050e63e24a05afcbb9b10dd386fdec6c7d6
fleepers <57987329+fleepers@users.noreply.github.com>
2023-03-07 15:19:49 +0000

    Create packages.sh

---
+++ packages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+pacman -S git rofi ttf-font-awesome ttf-material-design-icons python3 firefox nitrogen thunar alacritty neofetch discord
+cd /opt
+git clone https://aur.archlinux.org/yay-git.git
+chown -R tt:tt ./yay-git
+cd yay-git
+makepkg -si
+cd ~
+git clone https://github.com/fleepers/configfiles/
+mkdir ~/.config/alacritty
+cp configfiles/alacritty.yml ~/.config/alacritty
+rm -r ~/.config/i3
+cp -r configfiles/i3 ~/.config
