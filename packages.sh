2b0fd4b2b199e9b4c65d2bcb4334c05f89bb0d1f
fleepers <57987329+fleepers@users.noreply.github.com>
2023-03-07 16:11:32 +0000

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-pacman -S rofi python3 firefox nitrogen thunar alacritty neofetch discord
+sudo pacman -S rofi ttf-font-awesome python3 firefox nitrogen thunar alacritty neofetch discord
 mkdir ~/.config/alacritty
 cp alacritty.yml ~/.config/alacritty
 rm -r ~/.config/i3
 cp -r i3 ~/.config
-echo "ttf-font-awesome & ttf-material-design-icons"
