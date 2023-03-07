525e095841f8adf67f2c839820ebe31d6920880a
fleepers <57987329+fleepers@users.noreply.github.com>
2023-03-07 16:30:28 +0000

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -5,3 +5,11 @@ cp alacritty.yml ~/.config/alacritty
 rm -r ~/.config/i3
 cp -r i3 ~/.config
 sudo chmod +x ~/.config/i3/scripts/*
+mkdir /tmp/yay
+cd /tmp/yay
+curl -OJ 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay'
+makepkg -si
+cd
+rm -rf /tmp/yay
+yay --version
+yay -S ttf-material-design-icons
