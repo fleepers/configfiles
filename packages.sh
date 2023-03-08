e9fd8aafbf5e8052faf73eccfe25bf2dbcaf1845
fleepers <57987329+fleepers@users.noreply.github.com>
2023-03-08 16:09:07 +0000

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo pacman -S rofi ttf-font-awesome python3 firefox nitrogen thunar alacritty neofetch discord pavucontrol picom code gamemode lib32-gamemode arandr steam
+sudo pacman -S rofi ttf-font-awesome python3 firefox nitrogen thunar alacritty neofetch discord pavucontrol picom code gamemode lib32-gamemode arandr pamixer steam
 mkdir ~/wp
 cp wp.jpg ~/wp
 mkdir ~/.config/alacritty
@@ -9,6 +9,7 @@ cp -r i3 ~/.config
 sudo chmod +x ~/.config/i3/scripts/*
 mkdir -p ~/.local/share/rofi/themes/
 cp squared-material-red.rasi ~/.local/share/rofi/themes/
+cp config.rasi ~/.config/rasi/
 mkdir /tmp/yay
 cd /tmp/yay
 curl -OJ 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay'
