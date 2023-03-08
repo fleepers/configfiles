a9f79485191bfb49ef3882554104618a968db21a
fleepers <57987329+fleepers@users.noreply.github.com>
2023-03-08 16:28:58 +0000

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo pacman -S rofi ttf-font-awesome python3 firefox nitrogen thunar alacritty neofetch discord pavucontrol picom code gamemode lib32-gamemode arandr pamixer steam
+sudo pacman -S rofi ttf-font-awesome python3 firefox nitrogen thunar libreoffice-fresh alacritty neofetch discord pavucontrol picom code gamemode lib32-gamemode arandr pamixer steam nvidia-settings
 mkdir ~/wp
 cp wp.jpg ~/wp
 mkdir ~/.config/alacritty
@@ -10,6 +10,7 @@ sudo chmod +x ~/.config/i3/scripts/*
 mkdir -p ~/.local/share/rofi/themes/
 cp squared-material-red.rasi ~/.local/share/rofi/themes/
 cp config.rasi ~/.config/rasi/
+cp monitor.sh ~/.screenlayout
 mkdir /tmp/yay
 cd /tmp/yay
 curl -OJ 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay'
@@ -18,5 +19,6 @@ cd
 rm -rf /tmp/yay
 yay --version
 yay -S ttf-material-design-icons
+yay -S ttf-ms-win10-auto
 yay -S spotify
 yay -S teams-for-linux
