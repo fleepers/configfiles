6c108d0cd510beaa5f13027869c3ae9a1fed1beb
fleepers <57987329+fleepers@users.noreply.github.com>
2023-03-09 15:07:33 +0000

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo pacman -S rofi ttf-font-awesome python3 firefox nitrogen thunar libreoffice-fresh p7zip alacritty neofetch lxappearance discord pavucontrol picom code samba smbclient gvfs-smb gamemode lib32-gamemode arandr pamixer steam flatpak nvidia-settings
+sudo pacman -S rofi ttf-font-awesome python3 firefox nitrogen thunar cpupower libreoffice-fresh p7zip alacritty neofetch lxappearance discord pavucontrol picom code samba smbclient gvfs-smb gamemode lib32-gamemode arandr pamixer steam flatpak nvidia-settings
 mkdir ~/wp
 cp wp.jpg ~/wp
 mkdir ~/.themes
