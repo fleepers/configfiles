a59a447bc6879e26b9a88ced864b4f26dcb2ce2c
Trevor Teven <57987329+fleepers@users.noreply.github.com>
2023-07-12 14:10:03 +0100

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo pacman -S rofi ttf-font-awesome python3 firefox swaybg thunar gamescope cpupower libreoffice-fresh p7zip alacritty neofetch lxappearance qt5-wayland qt6-wayland discord pavucontrol code samba smbclient gvfs-smb gamemode lib32-gamemode wine winetricks wine-mono mono pamixer steam flatpak
+sudo pacman -S rofi ttf-font-awesome python3 firefox nitrogen thunar cpupower libreoffice-fresh p7zip alacritty neofetch lxappearance discord pavucontrol code samba smbclient gvfs-smb gamemode lib32-gamemode wine winetricks wine-mono mono arandr pamixer steam flatpak
 sudo cpupower frequency-set -g performance
 mkdir ~/wp
 cp wp.jpg ~/wp
@@ -7,9 +7,9 @@ mkdir ~/.themes
 cp theme ~/.themes
 mkdir ~/.config/alacritty
 cp alacritty.yml ~/.config/alacritty
-rm -r ~/.config/sway
-cp -r sway ~/.config
-sudo chmod +x ~/.config/sway/scripts/*
+rm -r ~/.config/i3
+cp -r i3 ~/.config
+sudo chmod +x ~/.config/i3/scripts/*
 mkdir -p ~/.local/share/rofi/themes/
 cp squared-material-red.rasi ~/.local/share/rofi/themes/
 mkdir ~/.config/rofi
@@ -29,3 +29,6 @@ yay -S ttf-ms-win10-auto
 yay -S p7zip-gui
 yay -S spotify
 yay -S teams-for-linux
+yay -S davinci-resolve-studio
+yay -S amdgpu-pro-oglp
+yay -S opencl-amd
